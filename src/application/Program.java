af5d6a82200952866ce6d7cc2f5c554c02aeefb1
Gerson pas <sobrinho.gerson@gmail.com>
2021-01-26 17:52:22 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import entities.Client;
+
+public class Program {
+    public static void main(String[] args) {
+
+        String a = "Maria";
+        String b = "Alex";
+        System.out.println(a.equals(b));
+        System.out.println(a == b);
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+
+        Client c1 = new Client("Maria", "maria@gmail.com");
+        Client c2 = new Client("Maria", "maria@gmail.com");
+
+        System.out.println(c1.hashCode() );
+        System.out.println(c2.hashCode() );
+        System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2); // compara as referencias não o conteúdo da memória.
+
+
+
+
+
+
+    }
+}
